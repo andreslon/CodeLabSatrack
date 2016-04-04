bbc679143457d17810d02e13c261df2e112a1bd0
Andrés Londoño <andreslon@outlook.com>
2016-04-04 09:44:55 -0500

    Proyecto base Xamarin Forms, Creación de diseño en vista de Login

---
+++ LabSatrack/LabSatrack.Cross/LabSatrack.Cross/Views/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace LabSatrack.Cross.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            btnIngresar.Clicked += BtnIngresar_Clicked;
+
+        }
+
+        async private void BtnIngresar_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Regard", "Hi everyone :) ", "Ok");
+        }
+    }
+}
